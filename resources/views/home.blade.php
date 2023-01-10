f95fd9c5a3f3dadf02cbd3daaf16eeda7ecf3dbe
MirkoFarina <MirkoFarina@outlook.com>
2023-01-10 18:16:02 +0100

    [print] all ticket

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>
-        Home
-    </h1>
+    <div class="container">
+        <h1>
+            Treni Disponibili
+        </h1>
+        <div class="row">
+            @foreach ($trains as $train)
+                <div class="col-4 my-2">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <span>
+                                Codice Treno: {{ $train->trains_code }}
+                            </span>
+                            <span>
+                                Orario di Partenza: {{$train->departure_time}}
+                            </span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title"> Da: {{$train->departure_station}} </h5>
+                            <h5 class="card-title"> A: {{$train->arrival_station}} </h5>
+                            <p class="card-text">Arrivo previsto: {{ $train->arrival_time }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,11 @@
 <header>
     <nav>
-        H
+        <ul class="d-flex list-unstyled px-5">
+            <li>
+                <a class="text-black text-decoration-none" href=" {{route('home')}} ">
+                    HOME
+                </a>
+            </li>
+        </ul>
     </nav>
 </header>
