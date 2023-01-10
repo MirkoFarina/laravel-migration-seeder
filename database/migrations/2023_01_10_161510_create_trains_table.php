80269f8661544bcc7a42954e7d2cb3a7e3f6e251
MirkoFarina <MirkoFarina@outlook.com>
2023-01-10 17:37:40 +0100

    [create] table trains in db

---
+++ database/migrations/2023_01_10_161510_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 120);
+            $table->string('departure_station', 120);
+            $table->string('arrival_station', 120);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->tinyInteger('trains_code')->unsigned();
+            $table->tinyInteger('number_coaches')->unsigned();
+            $table->boolean('is_ontime')->default(1);
+            $table->boolean('is_available')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
